d9a5e6fa0b9691748aa4ac5e37c8581b7589387a
Revathi-1359 <revathilahari31@gmail.com>
2026-03-28 22:30:44 +0530

    Add files via upload
    Week1 Day1 Assignments

---
+++ Day1/HelloTestLeaf.java
@@ -0,0 +1,12 @@
+package Week1.Day1;
+
+public class HelloTestLeaf {
+public static void main(String[] args) {
+    float browserVersion = 0.02f;
+    String browserName = "Firefox";
+    boolean isVissible = true;
+    int releaseYear = 1998;
+    char browserLogo = 'f';
+    System.out.println("browserVersion:"+browserVersion+" ,"+"broserName:"+browserName+" , "+"isVissible:" +isVissible+" , "+"releaseYear:"+releaseYear+" , "+"browserLogo:"+browserLogo);
+}
+}
---
+++ Day1/HelloTestleaf.java
@@ -0,0 +1,7 @@
+package week1.Day1;
+
+public class HelloTestleaf {
+public static void main(String[] args) {
+    System.out.println("Welcome to Testleaf");
+}
+}
